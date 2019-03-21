790fdec223a43bb5a542dae8cffe2da6088f3792
Saket Narayan <saket@uncommon.is>
2019-03-21 11:41:45 +0000

    Speed up showing of Progress tab by instantiating it on app start

--- app/src/main/java/org/simple/clinic/home/HomeScreen.kt
+++ app/src/main/java/org/simple/clinic/home/HomeScreen.kt
@@ -2,11 +2,11 @@ package org.simple.clinic.home
 
 import android.annotation.SuppressLint
 import android.content.Context
-import androidx.viewpager.widget.ViewPager
 import android.util.AttributeSet
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RelativeLayout
+import androidx.viewpager.widget.ViewPager
 import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
@@ -52,6 +52,13 @@ class HomeScreen(context: Context, attrs: AttributeSet) : RelativeLayout(context
     rootLayout.hideKeyboard()
 
     viewPager.adapter = HomePagerAdapter(context)
+
+    // The WebView in "Progress" tab is expensive to load and freezes the app for
+    // a good 1-2s when the tab is clicked. Instantiating it when the ViewPager is
+    // starting solves it at the expense of (theoretically) delaying the app startup
+    // slightly. The delay seems to be unnoticeable in our limited testing so this
+    // feels harmless.
+    viewPager.offscreenPageLimit = HomeTabs.PATIENT.ordinal - HomeTabs.REPORTS.ordinal
   }
 
   private fun screenCreates() = Observable.just(ScreenCreated())
